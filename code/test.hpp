29dfc43e134b15db8b5182d2ef968e6b82591915
solym <ymwh@foxmail.com>
2019-06-05 14:17:19 +0800

    顺序迭代查询测试

---
+++ code/test.hpp
@@ -0,0 +1,119 @@
+#ifndef __TEST_HPP__
+#define __TEST_HPP__
+
+#include <time.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <vector>
+#include <string>
+
+class Timer {
+    clock_t _s;
+    clock_t _e;
+public:
+    Timer()
+        :_s(clock()), _e(0) {}
+    inline clock_t Start() {
+        return _s = clock();
+    }
+    inline clock_t Stop() {
+        return _e = clock();
+    }
+    inline double elapsed() const {
+        return double(_e - _s) * 1000.0 / CLOCKS_PER_SEC;
+    }
+    inline double elapsed_sec() const {
+        return double(_e - _s) / CLOCKS_PER_SEC;
+    }
+};
+
+struct Rect {
+    double x0, x1;
+    double y0, y1;
+};
+inline bool Intersect(const Rect& r1, const Rect& r2)
+{
+    return !((r1.x0 > r2.x1) ||
+        (r1.x1 < r2.x0) ||
+        (r1.y0 > r2.y1) ||
+        (r1.y1 < r2.y0));
+}
+struct Node {
+    uint64_t    id;        // 节点ID
+    std::string url;       // 影像路径
+    Rect        box;       // 外包框
+    time_t      dtime;     // 数据时间
+    int         sat;       // 卫星
+};
+
+class TestBase {
+protected:
+    void resetRand(int seed)
+    {
+        srand(seed);
+    }
+    void getNextData(Rect& r, time_t& dtime, int& sat) const
+    {
+        // 生成大小在0.002~0.2度大小的矩形(绝大部分影像的大小)
+        // 0.002度约为222米，0.2度 22.26千米
+        // 高与宽的长度差距在上下70%以内
+        Node n;
+        r.x0 = -180 + double(rand()) / double(RAND_MAX) * 359.8;
+        r.x0 = -90 + double(rand()) / double(RAND_MAX) * 179.8;
+        double w = 0.002 + double(rand()) / double(RAND_MAX) * 0.1998;
+        double h = w * (1.0 + double(0.01 * (70 - rand() % 141)));
+        r.x1 = r.x0 + w;
+        r.y1 = r.y0 + h;
+        // 时间控制在1990年1月1日到2019年1月1日
+        dtime = 631123200 + rand() % 915148800;
+        // 卫星限制在0-5
+        sat = rand() % 6;
+    }
+
+    void getQueryArg(Rect& r,time_t& t0,time_t& t1,int& sat) const
+    {
+        // 查询框在 0.0023~5.625度（约为度网格5~16级）
+        double w = 0.0023 + double(rand()) * 5.625 / double(RAND_MAX);
+        r.x0 = -180 + double(rand()) / double(RAND_MAX) * (360.0 - w);
+        r.y0 = -90 + double(rand()) / double(RAND_MAX) * (180.0 - w);
+        r.x1 = r.x0 + w;
+        r.y1 = r.y0 + w;
+        // 时间控制在1个月(2592000)~5年(155520000)之间
+        time_t el = 2592000 + (rand() % 152928000);
+        t0 = 631123200 + (rand() % (915148800-el));
+        t1 = t1 + el;
+        // 卫星
+        sat = rand() % 6;
+    }
+public:
+    virtual void init(size_t count) = 0;
+    virtual void insert(size_t count) = 0;
+    virtual void query(size_t count) = 0;
+};
+
+
+
+template<typename T>
+void TestRun(size_t count)
+{
+    TestBase* p = new T;
+    Timer t;
+
+    p->init(count);
+
+    printf("\n\n@@@@@@@@@@@@@@@@----TestRun(insert(%llu))----@@@@@@@@@@@@@@@@\n", count);
+    t.Start();
+    p->insert(count);
+    t.Stop();
+    printf("\n-----TestRun(insert(%llu)) 耗时%lf-----\n", count, t.elapsed());
+
+    printf("\n------------TestRun(query(%llu))-----------\n", count);
+    t.Start();
+    p->query(count);
+    t.Stop();
+    printf("\n%%%%%%%%%%%%%%%%----TestRun(query(%llu)) 耗时%lf----%%%%%%%%\n", count, t.elapsed());
+}
+
+#endif //!__TEST_HPP__
+
---
+++ code/testiter.cpp
@@ -0,0 +1,118 @@
+#include "test.hpp"
+
+const int inserseed = 2019;
+const int queryseed = 78658;
+
+class TestIter :public TestBase {
+    std::vector<Node> data;
+public:
+    void init(size_t count)
+    {
+        data.reserve(count);
+    }
+    void insert(size_t count)
+    {
+        resetRand(inserseed);
+
+        Timer t;
+        double zsj = 0;
+        for (size_t i = 0; i < count; ++i) {
+            // 获取节点数据
+            Node n;
+            getNextData(n.box, n.dtime, n.sat);
+            n.url = std::to_string(i);
+            n.id = i;
+
+            // 插入计时
+            t.Start();
+            data.emplace_back(n);
+            t.Stop();
+            zsj += t.elapsed();
+        }
+        printf("插入 %llu 总耗时:%lf 毫秒\n", count, zsj);
+    }
+    void query(size_t count)
+    {
+        resetRand(queryseed);
+
+        Timer t;
+        double zsj = 0;
+        for (size_t i = 0; i < count; ++i) {
+            // 获取查询条件
+            Rect r;
+            time_t t0, t1;
+            int sat;
+            getQueryArg(r, t0, t1, sat);
+
+            // 查询计时
+            t.Start();
+            std::vector<size_t> result;
+            for (size_t i = 0; i < data.size(); ++i) {
+                Node& n = data[i];
+                if (Intersect(r, n.box)) {
+                    result.push_back(n.id);
+                }
+            }
+            t.Stop();
+            zsj += t.elapsed();
+
+            // 输出中间那次的查询条件和结果
+            if (i == count / 2) {
+                printf("查询条件:box[(%lf,%lf)-(%lf,%lf)],%lld<=dtime<=%lld,sat=%d\n查询结果(仅输出ID)：",
+                    r.x0, r.y0, r.x1, r.y1, t0, t1, sat);
+                for (auto& id : result) {
+                    printf("%llu,", id);
+                }
+                puts("\n");
+            }
+        }
+        printf("查询(仅范围) %llu 总耗时:%lf 毫秒\n", count, zsj);
+
+        resetRand(queryseed);
+        zsj = 0;
+        for (size_t i = 0; i < count; ++i) {
+            // 获取查询条件
+            Rect r;
+            time_t t0, t1;
+            int sat;
+            getQueryArg(r, t0, t1, sat);
+
+            // 查询计时
+            t.Start();
+            std::vector<size_t> result;
+            for (size_t i = 0; i < data.size(); ++i) {
+                Node& n = data[i];
+                if (Intersect(r, n.box) && 
+                    (n.dtime <= t1 && n.dtime >= t0) &&
+                    n.sat == sat) {
+                    result.push_back(n.id);
+                }
+            }
+            t.Stop();
+            zsj += t.elapsed();
+
+            // 输出中间那次的查询条件和结果
+            if (i == count / 2) {
+                printf("查询条件:box[(%lf,%lf)-(%lf,%lf)],%lld<=dtime<=%lld,sat=%d\n查询结果(仅输出ID)：",
+                    r.x0, r.y0, r.x1, r.y1, t0, t1, sat);
+                for (auto& id : result) {
+                    printf("%llu,", id);
+                }
+                puts("\n");
+            }
+        }
+        printf("查询(全条件) %llu 总耗时:%lf 毫秒\n", count, zsj);
+    }
+};
+
+
+
+int main()
+{
+    TestRun<TestIter>(10000);
+    TestRun<TestIter>(50000);
+    TestRun<TestIter>(100000);
+    TestRun<TestIter>(200000);
+    // TestRun<TestIter>(1000000);
+    return 0;
+}
